66945cae2a39902e08d76de89b574dc50dd7b654
Ariel Parra <darkaico@gmail.com>
2024-12-01 21:06:33 +0100

    feat: updating fonts

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ const App: React.FC = () => {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen">
       <ParticleCanvas />
-      <div className="relative z-10">
-        <h1 className="text-4xl font-bold mb-8 mt-8">Welcome to my GitHub page</h1>
+      <div className="relative z-10 flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-8 mt-8 text-center">Welcome to my GitHub page</h1>
         <RepoList />
       </div>
     </div>
