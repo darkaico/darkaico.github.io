b183354c6dd019f625221302ab4546d915bbb8f5
Ariel Parra <darkaico@gmail.com>
2024-12-01 21:53:49 +0100

    chore: adjust css

--- src/RepoList.tsx
+++ src/RepoList.tsx
@@ -10,14 +10,14 @@ interface Repo {
 }
 
 const RepoItem: React.FC<{ repo: Repo }> = ({ repo }) => (
-  <div className="p-2 mb-4 bg-gray-800 rounded-lg shadow-md">
+  <div className="p-4 mb-4 bg-gray-800 rounded-lg shadow-md max-w-xs mx-auto flex flex-col">
     <h3 className="text-lg font-bold truncate">{repo.name}</h3>
-    <p className="text-gray-300 text-sm">{repo.description || "No description available."}</p>
+    <p className="text-gray-300 text-sm mt-1">{repo.description || "No description available."}</p>
     <a
       href={repo.html_url}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-blue-500 hover:underline mt-1 block"
+      className="text-blue-500 hover:underline mt-2 block"
     >
       Visit Repository
     </a>
@@ -48,15 +48,17 @@ const RepoList: React.FC = () => {
   const forkedRepos = repos.filter((repo) => repo.fork);
 
   return (
-    <div className="flex flex-col md:flex-row justify-between repo-container w-full">
-      <div className="w-full md:w-1/2 pr-4 mb-4 md:mb-0">
-        <h2 className="text-2xl font-bold text-green-400 mb-4">My Repositories</h2>
+    <div className="flex flex-col md:flex-row justify-center repo-container w-full">
+      <div className="w-full md:w-1/3 pr-4 mb-4 md:mb-0">
+        <h2 className="text-2xl font-bold text-green-400 mb-4 p-4 text-center">My Repositories</h2>
         {ownedRepos.map((repo) => (
           <RepoItem key={repo.id} repo={repo} />
         ))}
       </div>
-      <div className="w-full md:w-1/2 pr-4 mb-4 md:mb-0">
-        <h2 className="text-2xl font-bold text-yellow-400 mb-4">Forked Repositories</h2>
+      <div className="w-full md:w-1/3 pr-4 mb-4 md:mb-0">
+        <h2 className="text-2xl font-bold text-green-400 mb-4 p-4 text-center">
+          Forked Repositories
+        </h2>
         {forkedRepos.map((repo) => (
           <RepoItem key={repo.id} repo={repo} />
         ))}
